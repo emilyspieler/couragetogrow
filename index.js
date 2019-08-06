6a281b1474d84f74f5181b5e8c2dee3fbc7e9ce0
Emily Spieler <emilyspieler1@gmail.com>
2019-08-06 12:26:32 -0500

    intital upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document
+	.getElementByClassName("ae-8 fromBottom btn-md white")
+  .addEventListener("submit", function(e) {
+  	e.preventDefault();
+    window.location.href = "index.html";
+  });
